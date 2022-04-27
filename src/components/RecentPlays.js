7c978c12c0f40d6057a314b9629ce162f5309a69
markeymark32 <cmm0014@gmail.com>
2022-04-27 06:29:26 -0400

    Update RecentPlays.js

--- src/components/RecentPlays.js
+++ src/components/RecentPlays.js
@@ -87,7 +87,7 @@ function RecentPlays({ rtl, toggled, handleToggleSidebar }) {
           onToggle={handleToggleSidebar}
           width="270px"  >
           
-        <SidebarHeader>
+        <SidebarHeader className="border">
           <div className="home-recent-plays proximanova-bold-white-22px">
               Recent plays
           </div>
