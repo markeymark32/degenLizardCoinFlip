d6b9ac087579fb566197d30b3fa7a56202d60b3c
markeymark32 <cmm0014@gmail.com>
2022-04-27 05:34:51 -0400

    Update Rankings.js

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -9,11 +9,11 @@ function Rankings(props) {
       <div className="number-2 proximanova-regular-normal-white-18px">
         {number}
       </div>
-      <div className="signer proximanova-extra-normal-white-16px">
+      <div className="signer proximanova-bold-white-16px-3">
         {signer}
       </div>
       <div className="text proximanova-black-white-16px">
-        <span className="span-1 proximanova-black-white-16px-2">{amount}</span>
+        <span className="span-1 proximanova-bold-white-16px-3">{amount}</span>
         <span className="span-1 proximanova-black-green-16px greenColor"> Ⓝ</span>
       </div>
     </div>
